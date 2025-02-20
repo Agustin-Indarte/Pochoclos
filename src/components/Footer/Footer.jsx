181160b5013109dc000bd13be0db2030b19d905f
Tomas Escudero <tomyesq04@gmail.com>
2025-02-20 20:28:52 -0300

    faltan agregar los iconos de redes y sus estilos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import './Footer.css'
+
 function Footer() {
   return (
     <div className='footer'>
-      <img src="" alt="" />
-      <p></p>
+      <img className='img-footer' src="public\Logo-Pochoclos.png" alt="logo de pochoclos" />
+      <p className='derechos'>2025 Pochoclos. Todos los derechos reservados.</p>
       <div className='redes'>
-        
+
       </div>
     </div>
   )
