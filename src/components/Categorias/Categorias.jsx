093ac023bf5b1424fa6174cff403f70f89e85fc0
Tomas Escudero <tomyesq04@gmail.com>
2025-02-21 14:17:19 -0300

    estilos de categorias terminados

--- src/components/Categorias/Categorias.jsx
+++ src/components/Categorias/Categorias.jsx
@@ -7,20 +7,9 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper/modules";
 
-// const peliculas = [
-//   { id: 1, titulo: "Inception", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 2, titulo: "Interstellar", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 3, titulo: "Dune", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 4, titulo: "The Matrix", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 5, titulo: "Inception", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 6, titulo: "Interstellar", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 7, titulo: "Dune", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-//   { id: 8, titulo: "The Matrix", imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYhhLmuRYWMnB-0d5980M8nryiLenEGO3rLQ&s" },
-// ];
-
 const CarruselPeliculas = () => {
     const [peliculas, setPeliculas] = useState([]);
-    const apiKey = 'e845bcd33e2571e0313cbf204469c4fc'; // Reemplaza con tu API Key real
+    const apiKey = 'e845bcd33e2571e0313cbf204469c4fc'; 
     const url_api = 'https://api.themoviedb.org/3'
     const img_path = 'https://image.tmdb.org/t/p/original'
     const url_img = 'https://image.tmdb.org/t/p/original'
@@ -36,12 +25,9 @@ const CarruselPeliculas = () => {
                 }
                 const datos = await respuesta.json();
                 console.log(datos)
-                setPeliculas(datos.results.slice(0, 10)); // Limita a 10 películas
+                setPeliculas(datos.results.slice(0, 20));
             } catch (error) {
                 console.error('Error al obtener películas:', error);
-                // Manejo de errores: Mostrar un mensaje al usuario o una imagen de error
-                // Por ejemplo:
-                // alert("Error al cargar las películas. Por favor, inténtalo de nuevo más tarde.");
             }
         };
 
@@ -56,10 +42,10 @@ const CarruselPeliculas = () => {
       navigation
       pagination={{ clickable: true }}
       breakpoints={{
-        320: { slidesPerView: 1 },  // En pantallas pequeñas, 1 imagen
-        768: { slidesPerView: 2 },  // En tablets, 2 imágenes
-        1024: { slidesPerView: 3 }, // En pantallas medianas, 3 imágenes
-        1280: { slidesPerView: 4 }, // En pantallas grandes, 4 imágenes
+        320: { slidesPerView: 1 }, 
+        768: { slidesPerView: 2 },  
+        1024: { slidesPerView: 3 }, 
+        1280: { slidesPerView: 4 }, 
       }}
       modules={[Pagination, Navigation]}
       className="mySwiper"
@@ -67,7 +53,7 @@ const CarruselPeliculas = () => {
       {peliculas.map((pelicula) => (
         <SwiperSlide key={pelicula.id}>
           <img src={`${url_img + pelicula.poster_path}`} alt={pelicula.title} className='img-peliculas' />
-          <h3 style={{ textAlign: "center" }}>{pelicula.title}</h3>
+          <h3 className='titulo-peli'>{pelicula.title}</h3>
         </SwiperSlide>
       ))}
     </Swiper>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,6 +11,7 @@ function Home() {
     <Categorias/>
     <Categorias/>
     <Categorias/>
+
     
     <Footer/>
     </>
