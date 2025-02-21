4e0e1e76551261cfcfa7ea2d0220187e70d85377
Tomas Escudero <tomyesq04@gmail.com>
2025-02-21 15:09:17 -0300

    estilos del footer terminados, solo falta establecer las rutas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,34 @@
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faXTwitter, faInstagram, faTiktok} from '@fortawesome/free-brands-svg-icons';
+
 
 function Footer() {
   return (
     <div className='footer'>
       <img className='img-footer' src="public\Logo-Pochoclos.png" alt="logo de pochoclos" />
-      <p className='derechos'>2025 Pochoclos. Todos los derechos reservados.</p>
+      <p className='derechos'>© 2025 Pochoclos. Todos los derechos reservados.</p>
       <div className='redes'>
+        <a href="" target="_blank" style={{ margin: '0 10px' }}>
+          <i className="fab fa-facebook" style={{ fontSize: '30px' }}>
+          <FontAwesomeIcon icon={faFacebook} style={{ fontSize: '30px' }} />
+          </i>
+        </a>
+        <a href="" target="_blank" style={{ margin: '0 10px' }}>
+          <i className="fab fa-xtwitter" style={{ fontSize: '30px' }}>
+          <FontAwesomeIcon icon={faXTwitter} style={{ fontSize: '30px' }} />
+          </i>
+        </a>
+        <a href="" target="_blank" style={{ margin: '0 10px' }}>
+          <i className="fab fa-instagram" style={{ fontSize: '30px' }}>
+          <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '30px' }} />
+          </i>
+        </a>
+        <a href="" target="_blank" style={{ margin: '0 10px' }}>
+          <i className="fab fa-tiktok" style={{ fontSize: '30px' }}>
+          <FontAwesomeIcon icon={faTiktok} style={{ fontSize: '30px' }} />
+          </i>
+        </a>
 
       </div>
     </div>
