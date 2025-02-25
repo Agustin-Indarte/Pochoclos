879a349595725b278033d028ae1ba99336f15bc3
Agustin Indarte <aindarte111@gmail.com>
2025-02-25 20:51:54 -0300

    Merge BannerInicio y Dispositivos en PaginaInicial

--- src/components/Dispositivos/Dispositivos.jsx
+++ src/components/Dispositivos/Dispositivos.jsx
@@ -7,9 +7,9 @@ function Dispositivos() {
       <div className="text-container">
         <h1>Disfrutalo cuando y donde quieras</h1>
         <h4>Ya sea en tu celular, tablet, computadora o TV.</h4>
-        <img src="public\dispositivos.png" alt="imagen de dispositivos" />
-        <img className='pochoclosizq' src="public\pochoclos1.png" alt="pochoclos dispersos del lado izquierdo" />
-        <img className='pochoclosder' src="public\pochoclos2.png" alt="pochoclos dispersos del lado derecho" />
+        <img src="/dispositivos.png" alt="imagen de dispositivos" />
+        {/* <img className='pochoclosizq' src="public\pochoclos1.png" alt="pochoclos dispersos del lado izquierdo" />
+        <img className='pochoclosder' src="public\pochoclos1.png" alt="pochoclos dispersos del lado derecho" /> */}
       </div>
     </>
   );
