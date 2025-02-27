7dc3ea74daa29dfac54dd6dd1560439f6f3b0689
martinsosa5 <martinsoosaa31@gmail.com>
2025-02-27 18:20:49 -0300

    agregue el componente HeaderAdmin estructurar mejor el codigo y no tener todo en la pagina administracion

--- src/pages/Administracion/Administracion.jsx
+++ src/pages/Administracion/Administracion.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import './Administracion.css';
+import HeaderAdmin from '../../components/Administracion/HeaderAdmin/HeaderAdmin';
+
 
 function Administracion() {
   return (
-        <div className='container contenedorHeader m-5 align-items-center justify-content-center'>
-            <div className='row'>
-                <h1 className='col-12'>ADMINISTRACIÓN</h1>
-            </div>
-            <div className='row mt-5'>
-                <h2 className='col-8 '>LISTA DE PELICULAS</h2>
-                <button className='col-4 btnNewMovie'>NUEVA PELICULA</button>
-            </div>
-        </div>
+        <>
+            <HeaderAdmin/>
+        </>
+        
   )
 }
 
