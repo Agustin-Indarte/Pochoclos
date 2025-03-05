2041b9771fc9557dd32d8f00d426755c0b1c7718
Agustin Indarte <aindarte111@gmail.com>
2025-03-05 20:50:11 -0300

    correcciones en la validacion y limitacion de los formularios de login y registro

--- src/components/RegistroYSesion/FormulariodeRegistro/FormulariodeRegistro.jsx
+++ src/components/RegistroYSesion/FormulariodeRegistro/FormulariodeRegistro.jsx
@@ -9,33 +9,57 @@ import { useNavigate, Link } from 'react-router-dom';
 import { auth } from '../../../Firebase';
 
 const SignUpSchema = Yup.object().shape({
-  email: Yup.string().email('Email inválido').required('Requerido'),
-  username: Yup.string().required('Requerido'),
-  password: Yup.string().min(8, 'Contraseña muy corta').required('Requerido'),
-  creditCard: Yup.string().required('Requerido'),
-  securityCode: Yup.string().min(3, 'Código inválido').required('Requerido'),
+  email: Yup.string()
+    .email('Email inválido')
+    .max(29, 'Máximo 30 caracteres')
+    .required('Requerido'),
+
+  username: Yup.string()
+    .max(9, 'Máximo 10 caracteres')
+    .required('Requerido'),
+
+  password: Yup.string()
+    .min(8, 'Debe tener al menos 8 caracteres')
+    .max(15, 'Máximo 16 caracteres')
+    .required('Requerido'),
+
+    creditCard: Yup.string()
+    .matches(/^\d{14,15}$/, 'Debe tener entre 14 y 15 dígitos y solo números')
+    .required('Requerido'),
+
+  securityCode: Yup.string()
+    .matches(/^\d{3,4}$/, 'Debe tener entre 3 y 4 dígitos y solo números')
+    .required('Requerido'),
 });
 
-const SignUpForm  = () => {
-  const navigate = useNavigate(); 
 
-  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+const SignUpForm = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (values, formikHelpers) => {
+    const { setSubmitting, resetForm } = formikHelpers;
+
     try {
-      
+
       const templateParams = {
         plan: values.plan,
         email: values.email,
-        username: values.username,
-        
+        username: values.username
       };
 
-      const serviceId = 'service_sm1p9wy';
-      const templateId = 'template_l6dc3kd';
-      const userId = 'uCEXyRA4tN57jmK5r';
+      const serviceId = 'service_bfl0fcv';
+      const templateId = 'template_2fjnzta';
+      const userId = 'TmbimF1gBOKCCqIGU';
+      console.log("Datos que se enviarán a EmailJS:", templateParams)
+      await emailjs.send(serviceId, templateId, templateParams, userId)
+        .then(response => {
+          console.log("Email enviado correctamente:", response);
+        })
+        .catch(error => {
+          console.error("Error enviando email:", error);
+        });
 
-      await emailjs.send(serviceId, templateId, templateParams, userId);
 
-    
       await createUserWithEmailAndPassword(auth, values.email, values.password);
 
       Swal.fire({
@@ -44,8 +68,8 @@ const SignUpForm  = () => {
         text: 'Te has suscrito correctamente. Revisa tu correo electrónico.',
       });
 
-      resetForm(); 
-      navigate('/login'); 
+      resetForm();
+      navigate('/login');
 
     } catch (error) {
       console.error("Error al suscribirse:", error);
@@ -73,11 +97,11 @@ const SignUpForm  = () => {
       });
     }
 
-}
-  
+  }
+
   return (
     <Container className="rounded" style={{ backgroundColor: '#CFCFCF', width: '100%', maxWidth: '350px' }}>
-      <h1 style={{ textAlign: 'center', fontSize: '20px', color: '#D90429', fontWeight: 'bold', marginTop:"15px" }}>¡Elige tu plan pochoclero!</h1>
+      <h1 style={{ textAlign: 'center', fontSize: '20px', color: '#D90429', fontWeight: 'bold', marginTop: "15px" }}>¡Elige tu plan pochoclero!</h1>
       <Formik
         initialValues={{
           plan: '',
@@ -106,35 +130,38 @@ const SignUpForm  = () => {
             </Row>
             <Row className="justify-content-center mb-3">
               <Col md={12}>
-                <Field name="email" type="email" placeholder="Email" className={`form-control ${touched.email && errors.email ? 'is-invalid' : ''}`} />
+                <Field name="email" type="email" placeholder="Email" maxLength={30} className={`form-control ${touched.email && errors.email ? 'is-invalid' : ''}`} />
                 {touched.email && errors.email ? <div className="invalid-feedback">{errors.email}</div> : null}
               </Col>
             </Row>
             <Row className="justify-content-center mb-3">
               <Col md={12}>
-                <Field name="username" placeholder="Usuario" className={`form-control ${touched.username && errors.username ? 'is-invalid' : ''}`} />
+                <Field name="username" placeholder="Usuario" maxLength={10} className={`form-control ${touched.username && errors.username ? 'is-invalid' : ''}`} />
                 {touched.username && errors.username ? <div className="invalid-feedback">{errors.username}</div> : null}
               </Col>
             </Row>
             <Row className="justify-content-center mb-3">
               <Col md={12}>
-                <Field name="password" type="password" placeholder="Contraseña" className={`form-control ${touched.password && errors.password ? 'is-invalid' : ''}`} />
+                <Field name="password" type="password" placeholder="Contraseña" maxLength={16} className={`form-control ${touched.password && errors.password ? 'is-invalid' : ''}`} />
                 {touched.password && errors.password ? <div className="invalid-feedback">{errors.password}</div> : null}
               </Col>
             </Row>
             <Row className="justify-content-center mb-3">
               <Col xs={12} sm={6}>
-                <Field name="creditCard" placeholder="Tarjeta de Crédito" className={`form-control ${touched.creditCard && errors.creditCard ? 'is-invalid' : ''}`} />
+                <Field name="creditCard" placeholder="Tarjeta de Crédito" maxLength={16} className={`form-control ${touched.creditCard && errors.creditCard ? 'is-invalid' : ''}`} />
                 {touched.creditCard && errors.creditCard ? <div className="invalid-feedback">{errors.creditCard}</div> : null}
               </Col>
               <Col xs={12} sm={6}>
-                <Field name="securityCode" placeholder="Código de Seguridad" className={`form-control ${touched.securityCode && errors.securityCode ? 'is-invalid' : ''}`} />
+                <Field type="password" name="securityCode" placeholder="Código de Seguridad" maxLength={4} className={`form-control ${touched.securityCode && errors.securityCode ? 'is-invalid' : ''}`} />
                 {touched.securityCode && errors.securityCode ? <div className="invalid-feedback">{errors.securityCode}</div> : null}
               </Col>
             </Row>
             <Row className="justify-content-center mb-3">
               <Col md={12} className="text-center">
-                <Button type="submit" variant="danger" className="w-100" onClick={handleSubmit} style={{ border: '4px solid white' }}>Suscribirse</Button>
+                <Button type="submit" variant="danger" className="w-100" style={{ border: '4px solid white' }}>
+                  Suscribirse
+                </Button>
+
                 <Button variant="danger" className="w-100 mt-2" onClick={handleGoogleLogin} style={{ border: '4px solid white' }}>Suscribirse con Google</Button>
                 <p style={{ marginTop: '1rem', color: '#D90429' }}>
                   Si ya te encuentras suscripto, <Link to="/login" style={{ color: '#AC011F' }}>Inicia Sesión</Link>
@@ -145,7 +172,8 @@ const SignUpForm  = () => {
         )}
       </Formik>
     </Container>
-  );};
+  );
+};
 
 export default SignUpForm;
 
--- src/components/RegistroYSesion/LoginForm/LoginForm.jsx
+++ src/components/RegistroYSesion/LoginForm/LoginForm.jsx
@@ -13,8 +13,15 @@ import { Link } from "react-router-dom";
 
 // Esquema de validación con Yup
 const schema = yup.object().shape({
-  email: yup.string().email("Correo inválido").required("El correo es obligatorio"),
-  password: yup.string().min(6, "La contraseña debe tener al menos 6 caracteres").required("La contraseña es obligatoria"),
+  email: yup.string()
+      .email('Email inválido')
+      .max(29, 'Máximo 30 caracteres')
+      .required('Requerido'),
+  
+    password: yup.string()
+      .min(8, 'Debe tener al menos 8 caracteres')
+      .max(15, 'Máximo 16 caracteres')
+      .required('Requerido'),
 });
 
 function LoginForm() {
@@ -61,7 +68,7 @@ function LoginForm() {
         paddingTop: '20px' // Ajusta el margen superior
       }}
     >
-      <Card style={{ backgroundColor: "#CFCFCF", width: '400px', height: '400px' }} className="p-4 rounded">
+      <Card style={{ backgroundColor: "#CFCFCF", width: '400px', height: '450px' }} className="p-4 rounded">
         <div className="text-center text-danger">
           <h1 className="fw-bold mt-0" style={{ fontSize: '18px' }}>¡Bienvenido a tu cine en casa!</h1>
           <h2 style={{ fontSize: '16px', color: '#151931' }}>Iniciá sesión y disfrutá de tus series y películas favoritas en cualquier momento. Prepará algo rico, acomodate y sumergite en el mejor entretenimiento desde tu hogar.</h2>
@@ -74,6 +81,7 @@ function LoginForm() {
               type="email"
               placeholder="Ingrese su correo"
               {...register("email")}
+              isInvalid={!!errors.email}
             />
             {errors.email && <p className="text-danger">{errors.email.message}</p>}
           </Form.Group>
@@ -82,7 +90,8 @@ function LoginForm() {
             <Form.Control
               type="password"
               placeholder="Ingrese su contraseña"
-              {...register("password")}
+              {...register("password")} 
+              isInvalid={!!errors.password}
             />
             {errors.password && <p className="text-danger">{errors.password.message}</p>}
           </Form.Group>
